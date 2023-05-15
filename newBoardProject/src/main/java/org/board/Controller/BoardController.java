e6cd562b48bd739c44ef5d5dd9126e597bb47a2d
ParkIlKwon <qkrdlfrnjs95@naver.com>
2023-05-15 16:28:01 +0900

    fa

--- newBoardProject/src/main/java/org/board/Controller/BoardController.java
+++ newBoardProject/src/main/java/org/board/Controller/BoardController.java
@@ -1,10 +1,23 @@
 package org.board.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 
+
+    @PostMapping()
+    @ResponseBody
+    public String writeBoard(){
+
+        
+
+        return "ok";
+    }
 
 
 }
