d03c855e1443610609e3c4aca4c2246f0aa4a660
KIMBEOMJIN <kimbj0117@gmail.com>
2023-05-15 13:20:19 +0900

    update member
    domain builder , controller

---
+++ newBoardProject/src/main/java/org/board/Controller/KimBoardController.java
@@ -0,0 +1,11 @@
+package org.board.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/kimBoard")
+public class KimBoardController {
+}
--- newBoardProject/src/main/java/org/board/domain/Member.java
+++ newBoardProject/src/main/java/org/board/domain/Member.java
@@ -1,20 +1,45 @@
 package org.board.domain;
 
+import lombok.*;
+import org.springframework.util.Assert;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
     private Long id; //멤버 pk
-
+    @NotNull
+    @Column(length = 20, unique = true)
     private String userId;
+    @NotNull
+    @Column(length = 20)
+    private String password;
+    @NotNull
+    @Column(length = 10, unique = true)
+    private String nickname;
+    @NotNull
+    private Role role;
 
-    private String pw;
+    @Builder(builderClassName = "ByMemberJoinBuilder", builderMethodName = "ByMemberJoinBuilder")
+    public Member(String userId, String password, String nickname, Role role){
+        Assert.hasText(userId, "userId must not be empty");
+        Assert.hasText(password,"password must not be empty");
+        Assert.hasText(nickname, "nickname must not be empty");
+        Assert.notNull(role,"role must not be null");
+        Assert.notNull(role,"role must not be empty");
 
-    private String Nickname;
-
-    private Role Role;
+        this.userId = userId;
+        this.password = password;
+        this.nickname = nickname;
+        this.role = role;
+    }
 
 }
