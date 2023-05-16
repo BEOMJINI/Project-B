7d5d2f814c461decd08fe15212b490a0f390dc54
KIMBEOMJIN <kimbj0117@gmail.com>
2023-05-16 15:57:03 +0900

    update static
    member, config . css / js / html

--- newBoardProject/src/main/java/org/board/controller/MemberController.java
+++ newBoardProject/src/main/java/org/board/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -21,6 +22,7 @@ public class MemberController {
         return "member/join";
     }
     @PostMapping("/join")
+    @ResponseBody
     public String test () {
         Member member = Member.ByMemberJoinBuilder()
                 .nickname("2")
